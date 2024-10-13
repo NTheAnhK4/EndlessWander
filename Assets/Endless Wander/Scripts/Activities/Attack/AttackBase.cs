62129dc2444f45b40f0dab00d7a288731c9dcd6c
Nguyen The Anh <nguyentheanh14102004@gmail.com>
2024-10-13 17:44:27 +0700

    Start attack

--- Assets/Endless Wander/Scripts/Activities/Attack/AttackBase.cs
+++ Assets/Endless Wander/Scripts/Activities/Attack/AttackBase.cs
@@ -1,6 +1,38 @@
 
 
+using UnityEngine;
+
 public class AttackBase : ActionBase
 {
-    
+    [SerializeField] protected Transform entity;
+    [SerializeField] protected float attackSpeed;
+    [SerializeField] protected float timer;
+    protected override void UpdateLogic()
+    {
+        base.UpdateLogic();
+        timer += Time.deltaTime;
+    }
+
+    protected virtual bool CanAttack()
+    {
+        if (timer >= attackSpeed)
+        {
+            timer = 0;
+            return true;
+        }
+
+        return false;
+    }
+
+    protected virtual void OnAttack()
+    {
+        
+    }
+
+    protected override void UpdatePhysis()
+    {
+        base.UpdatePhysis();
+        if (!CanAttack()) return;
+        OnAttack();
+    }
 }
--- Assets/Endless Wander/Scripts/Activities/Attack/RangedAttack.cs
+++ Assets/Endless Wander/Scripts/Activities/Attack/RangedAttack.cs
@@ -1,8 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class RangedAttack : AttackBase
 {
-   
+    [SerializeField] protected Transform bulletPrefab;
+    protected override void OnAttack()
+    {
+        base.OnAttack();
+        Debug.Log("Attacking");
+    }
+
+    protected override void ResetValue()
+    {
+        entity = transform.parent;
+        attackSpeed = 3;
+        timer = 0;
+    }
 }
